6edd22553ac82e11f241c25d0549e243c929e277
Yong <yong@linkweek.cn>
2018-08-23 23:58:35 +0800

    Add gzip.

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"linkweek.cn/link-blog/routes"
+	"github.com/gin-contrib/gzip"
 )
 
 func main() {
@@ -10,6 +11,7 @@ func main() {
 
 	app.Use(gin.Logger())
 	app.Use(gin.Recovery())
+	app.Use(gzip.Gzip(gzip.DefaultCompression))
 
 	app.Static("/assets", "./assets")
 	app.StaticFile("/favicon.ico", "./resources/favicon.ico")
